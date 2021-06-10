1b73c25d451603fd65126c3b730688df89307981
root <aricomputacao@gmail.com>
2021-06-10 00:29:03 -0300

    ADICIONANDO ORIGEM PERMITODA

--- src/main/java/com/algaworks/algamoney/config/property/AlgamoneyApiProperty.java
+++ src/main/java/com/algaworks/algamoney/config/property/AlgamoneyApiProperty.java
@@ -5,12 +5,25 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @ConfigurationProperties("algamoney")
 public class AlgamoneyApiProperty {
 	
+	private String origemPermitida = "http://localhost:800";
+	private String originPermitida2 = "http://localhost:8000";
+
 	private final Seguranca seguranca = new Seguranca();
-	
-	
 	
 	public Seguranca getSeguranca() {
 		return seguranca;
+	}
+
+    
+
+	public String getOrigemPermitida() {
+		return origemPermitida;
+	}
+
+
+
+	public void setOrigemPermitida(String origemPermitida) {
+		this.origemPermitida = origemPermitida;
 	}
 
 
